1114ba3b3391f5c6dc3a82ba04204c2592daaa5b
Richard <richard@stromer.org>
2024-01-08 11:31:44 +0100

    show path to config file

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,9 @@ fn main() -> io::Result<()> {
     let home_dir = dirs::home_dir().expect("Could not find home directory");
     println!("The home directory is {}", home_dir.display());
 
+    let config_path = home_dir.join(".config").join("gi.toml");
+    println!("The config path is {}", config_path.display());
+
     let current_dir = env::current_dir()?;
     println!("The current directory is {}", current_dir.display());
 
