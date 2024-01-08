ffefbbe2133374343ef15339e0757d10739ac8b0
Richard <richard@stromer.org>
2024-01-08 12:04:25 +0100

    parse git repository config file and extract sections

---
+++ src/git.rs
@@ -0,0 +1,29 @@
+use configparser::ini::Ini;
+use log::debug;
+use std::{env, fs, io, path::PathBuf};
+
+fn git_config_path() -> io::Result<PathBuf> {
+    let current_dir = env::current_dir()?;
+    debug!("The current directory is {}", current_dir.display());
+
+    let git_config_path = current_dir.join(".git").join("config");
+    debug!("The git config path is {}", git_config_path.display());
+
+    Ok(git_config_path)
+}
+
+pub fn git_extract_remotes() -> io::Result<()> {
+    let git_config_path = git_config_path().expect("Could not get git config path");
+
+    let mut git_config = Ini::new();
+
+    let contents = fs::read_to_string(git_config_path)?;
+    git_config
+        .read(contents)
+        .expect("Could not parse git config");
+
+    let sections = git_config.sections();
+    debug!("The sections are {:?}", sections);
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 mod config;
+mod git;
 use config::read_config;
+use git::git_extract_remotes;
 use log::debug;
 use std::{env, io};
 
@@ -14,6 +16,10 @@ fn main() -> io::Result<()> {
 
     let current_dir = env::current_dir()?;
     debug!("The current directory is {}", current_dir.display());
+
+    // let git_config = read_git_config()?;
+    // debug!("The git config is {}", git_config);
+    git_extract_remotes()?;
 
     Ok(())
 }
