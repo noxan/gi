3b1a548d11f1f80a2d5dfa851407b1c293b47a71
Richard <richard@stromer.org>
2024-01-08 11:30:51 +0100

    add dirs and show home dir

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ use std::{env, io};
 fn main() -> io::Result<()> {
     println!("Hello, world!");
 
+    let home_dir = dirs::home_dir().expect("Could not find home directory");
+    println!("The home directory is {}", home_dir.display());
+
     let current_dir = env::current_dir()?;
     println!("The current directory is {}", current_dir.display());
 
