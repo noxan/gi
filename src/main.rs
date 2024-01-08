520232f55f810ae6aa15f688b0345c8eff24a4a1
Richard <richard@stromer.org>
2024-01-08 12:06:22 +0100

    remove duplicate code and list git remotes

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod git;
 use config::read_config;
 use git::git_extract_remotes;
 use log::debug;
-use std::{env, io};
+use std::io;
 
 fn main() -> io::Result<()> {
     env_logger::init();
@@ -14,12 +14,8 @@ fn main() -> io::Result<()> {
     let token = config.github.token;
     debug!("The github access token is {}", token);
 
-    let current_dir = env::current_dir()?;
-    debug!("The current directory is {}", current_dir.display());
-
-    // let git_config = read_git_config()?;
-    // debug!("The git config is {}", git_config);
-    git_extract_remotes()?;
+    let git_remotes = git_extract_remotes()?;
+    debug!("The git remotes are {:?}", git_remotes);
 
     Ok(())
 }
