ef3676eafd87a54bad6e96b4dfc6b97f9323c598
Richard <richard@stromer.org>
2024-01-08 11:13:16 +0100

    list current directory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
+use std::{env, io};
+
+fn main() -> io::Result<()> {
     println!("Hello, world!");
+
+    let current_dir = env::current_dir()?;
+    println!("The current directory is {}", current_dir.display());
+
+    Ok(())
 }
